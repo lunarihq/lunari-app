d00214cf3ea801e6ddcf76eb6e94b2250b1166fd
Maribel Ferreira <maribelferreira@users.noreply.github.com>
2026-02-21 16:32:25 +0000

    Refactor Drop icon to support dynamic color and size properties. Update default size to 24 and implement theme-based color selection for improved visual consistency across the application.

--- components/icons/general/Drop.tsx
+++ components/icons/general/Drop.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { SvgProps } from 'react-native-svg';
 import DropSvg from './drop.svg';
+import { useTheme } from '../../../contexts/ThemeContext';
 
 interface IconProps extends SvgProps {
   size?: number;
+  color?: string;
 }
 
 export const DropIcon: React.FC<IconProps> = ({
-  size = 12,
+  size = 24,
+  color,
   ...props
 }) => {
-  return <DropSvg width={size} height={size} {...props} />;
+  const { isDark } = useTheme();
+  const iconColor = color || (isDark ? '#5F7CFF' : '#4B61C7');
+
+  return <DropSvg width={size} height={size} color={iconColor} {...props} />;
 };
